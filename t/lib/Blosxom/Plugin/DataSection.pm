78057a9a47959db8485daeefbcd9afee4d00932b
Ryo Anazawa <anazawa@cpan.org>
2012-07-10 18:00:43 +0900

    rename get_data_section() to data_section()

--- t/lib/Blosxom/Plugin/DataSection.pm
+++ t/lib/Blosxom/Plugin/DataSection.pm
@@ -6,8 +6,8 @@ use Data::Section::Simple;
 sub begin {
     my ( $class, $c, $conf ) = @_;
     my $reader = Data::Section::Simple->new( $c );
-    my $method = sub { $reader->get_data_section( $_[1] ) };
-    $c->add_method( get_data_section => $method );
+    my $data = $reader->get_data_section;
+    $c->add_method( data_section => sub { $data } );
 }
 
 1;
